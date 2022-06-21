30e9221e402049fc1798bd7400a4cc5e825ca681
Mutlucan Gökçukur <91282825+MutlucanGokcukur@users.noreply.github.com>
2022-06-21 22:08:52 +0300

    Add files via upload

---
+++ Constructor1/Constructor1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+
+namespace Deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arac a1 = new Arac();
+            a1.SatınAl(5000, 200, "Mavi");
+            a1.Yazdır();
+
+            Arac a2 = new Arac();
+            a2.SatınAl(4000, 300, "Siyah");
+            a2.Yazdır();
+
+            Arac a3 = new Arac();
+            a3.SatınAl(3000, 100, "Turkuaz");
+            a3.Yazdır();
+        }
+    }
+    class Arac
+    {
+        private int Ağırlık { get; set; }
+        private int SilindirHacim { get; set; }
+        private static int AracNO { get; set; }
+        private string Renk { get; set; }
+        private string Açıklama { get; set; }
+
+        public Arac()
+        {
+            AracNO++;
+            Açıklama = AracNO + ". Araç";
+            Console.WriteLine(Açıklama);
+            Renk = "Kırmızı";
+            SilindirHacim = 1600;
+            Ağırlık = 1000;
+        }
+        public Arac(string renk)
+        {
+            AracNO++;
+            Açıklama = AracNO + ". Araç";
+            Console.WriteLine(Açıklama);
+            Renk = renk;
+        }
+        public void SatınAl(int Ağırlık, int SilindirHacim, string Renk)
+        {
+            this.Renk = Renk;
+            this.SilindirHacim = SilindirHacim;
+            this.Ağırlık = Ağırlık;
+        }
+        public void Yazdır()
+        {
+            Console.WriteLine("Ağırlık: " + Ağırlık + "\nSilindir Hacmi: " + SilindirHacim + "\nRenk: " + Renk);
+        }
+    }
+}
---
+++ Constructor2/Constructor2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+
+namespace Deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arac a1 = new Arac(2000, 2010);
+            a1.VitesArttir(5);
+            a1.VergiHesapla();
+            a1.Yazdır();
+        }
+    }
+    class Arac
+    {
+        private int MotorHacim { get; set; }
+        private int ModelYılı { get; set; }
+        public int Hız { get; set; }
+        public int Vites { get; set; }
+        public int Vergi { get; set; }
+
+        public int motorhacim
+        {
+            get
+            {
+                return MotorHacim;
+            }
+            set { }
+        }
+        public int modelyıl
+        {
+            get
+            {
+                return ModelYılı;
+            }
+            set { }
+        }
+        public Arac(int motorhacim, int modelyıl)
+        {
+            this.MotorHacim = motorhacim;
+            this.ModelYılı = modelyıl;
+        }
+        public int VergiHesapla()
+        {
+            if (MotorHacim == 1600)
+            {
+                Vergi = MotorHacim * 10;
+            }
+            else if (MotorHacim == 2000)
+            {
+                Vergi = MotorHacim * 20;
+            }
+            else if (MotorHacim == 2500)
+            {
+                Vergi = MotorHacim * 2500;
+            }
+            return Vergi;
+        }
+        public int VitesArttir(int vites)
+        {
+            this.Vites = vites;
+            Hız = Vites * 20;
+            return Vites;
+        }
+        public void Yazdır()
+        {
+            Console.WriteLine("Motor Hacim: " + MotorHacim + "\nModel Yılı: " + ModelYılı + "\nHız: " + Hız + "\nVites: " + Vites + "\nVergi: " + Vergi);
+        }
+    }
+}
