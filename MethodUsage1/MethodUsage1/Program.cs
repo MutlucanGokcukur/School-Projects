062006accfcbd15eae39481047d415eb46468399
Mutlucan Gökçukur <91282825+SmartScream@users.noreply.github.com>
2022-06-21 21:51:16 +0300

    Add files via upload

---
+++ MethodUsage1/MethodUsage1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+
+namespace Deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kart k1 = new Kart();
+            k1.musteriAdi = "Mutlucan";
+            k1.KartNo = 123456789;
+            k1.bakiye = 1000;
+            k1.Yazdır();
+            k1.ParaCek(500);
+        }
+    }
+    class Kart
+    {
+        public int KartNo;
+        public string musteriAdi;
+        public int bakiye;
+
+        public Kart() { }
+
+        public int ParaCek(int miktar)
+        {
+            if (bakiye - miktar < 0)
+            {
+                Console.WriteLine("Çekilemez");
+            }
+            else
+            {
+                bakiye = bakiye - miktar;
+                Console.WriteLine("Yeni Bakiye: " + bakiye);
+            }
+            return bakiye;
+        }
+        public void Yazdır()
+        {
+            Console.WriteLine("Bakiye: " + bakiye);
+            Console.WriteLine("Kart No: " + KartNo);
+            Console.WriteLine("Müşteri Adı: " + musteriAdi);
+        }
+
+    }
+}
---
+++ MethodUsage2/MethodUsage2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+namespace Deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dolap d1 = new Dolap(3, 4, 5);
+            d1.Hesap();
+            d1.Yaz();
+        }
+    }
+    class Dolap
+    {
+        public decimal en;
+        public decimal boy;
+        public decimal derinlik;
+        public decimal hacim;
+
+        public Dolap(decimal _en, decimal _boy, decimal _derinlik)
+        {
+            en = _en;
+            boy = _boy;
+            derinlik = _derinlik;
+        }
+        public decimal Hesap()
+        {
+            hacim = en * boy * derinlik;
+            return hacim;
+        }
+        public void Yaz()
+        {
+            Console.WriteLine("En: " + en);
+            Console.WriteLine("Boy: " + boy);
+            Console.WriteLine("Derinlik: " + derinlik);
+            Console.WriteLine("Hacim: " + hacim);
+        }
+
+    }
+}
