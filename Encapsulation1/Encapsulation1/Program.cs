5909d0e780d6864ba36dd76e8b3bd64262f1901f
Mutlucan Gökçukur <91282825+MutlucanGokcukur@users.noreply.github.com>
2022-06-21 22:08:03 +0300

    Add files via upload

---
+++ Encapsulation1/Encapsulation1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+
+namespace Deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci o1 = new Ogrenci();
+            o1.tckimlik = "32345354354";
+            o1.ogrencino = "32343543";
+
+        }
+    }
+    class Ogrenci
+    {
+        public string adsoyad { get; set; }
+        private string TCKimlik;
+        private string OgrenciNO;
+
+        public string tckimlik
+        {
+            get
+            {
+                return TCKimlik;
+            }
+            set
+            {
+                if (value.ToString().Length == 11)
+                {
+                    TCKimlik = value;
+                    Console.WriteLine("TC Kimlik Numarası: " + value);
+                }
+                else
+                {
+                    Console.WriteLine("TC Kimlik 11 Haneden Oluşmalıdır.");
+                }
+            }
+        }
+        public string ogrencino
+        {
+            get
+            {
+                return OgrenciNO;
+            }
+            set
+            {
+                if (value.ToString().Length == 9)
+                {
+                    OgrenciNO = value;
+                    Console.WriteLine("Ogrenci NO: " + value);
+                }
+                else
+                {
+                    Console.WriteLine("Ogrenci Numarası 9 Haneden Oluşmalıdır");
+                }
+            }
+        }
+
+    }
+}
---
+++ Encapsulation2/Encapsulation2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+
+namespace Deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri m1 = new Musteri();
+            m1.telefonno = "1235456879";
+            m1.idno = "1235";
+        }
+    }
+    class Musteri
+    {
+        public string isim { get; set; }
+        private string TelefonNO;
+        private string IDNO;
+
+        public string telefonno
+        {
+            get
+            {
+                return TelefonNO;
+            }
+            set
+            {
+                if (value.ToString().Length == 10)
+                {
+                    TelefonNO = value;
+                    Console.WriteLine("Telefon Numarası: " + TelefonNO);
+                }
+                else
+                {
+                    Console.WriteLine("Telefon Numarası 10 Haneli Olmalıdır");
+                }
+            }
+        }
+        public string idno
+        {
+            get
+            {
+                return IDNO;
+            }
+            set
+            {
+                if (value.ToString().Length == 5)
+                {
+                    IDNO = value;
+                    Console.WriteLine("İD Numarası: " + IDNO);
+                }
+                else
+                {
+                    Console.WriteLine("İd Numarası 5 Haneli Olmalıdır");
+                }
+            }
+        }
+    }
+}
