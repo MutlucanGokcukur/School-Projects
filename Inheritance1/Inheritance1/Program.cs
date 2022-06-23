35b235b35b1478cc4c9c46d5bf80162e4245ff47
Mutlucan Gökçukur <91282825+MutlucanGokcukur@users.noreply.github.com>
2022-06-23 12:12:23 +0300

    Add files via upload

---
+++ Inheritance1/Inheritance1/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+
+namespace Deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arac a1 = new Arac();
+            a1.AracTür = "Audi";
+            a1.YapımYIL = 2022;
+            Console.WriteLine("--------------Araç--------------");
+            a1.OrtakYazdır();
+
+            otomobil o1 = new otomobil();
+            Console.WriteLine("--------------Otomobil--------------");
+            o1.ARACTUR = "Toyota";
+            o1.TEKERSAYI = 4;
+            o1.AGIRLIK = 2000;
+            o1.YapımYIL = 2021;
+            o1.OrtakYazdır();
+
+            otomobil o2 = new otomobil();
+            Console.WriteLine("--------------Otomobil--------------");
+            o1.ARACTUR = "Mercedes";
+            o1.YapımYIL = 2020;
+            o1.AGIRLIK = 1500;
+            o1.TEKERSAYI = 4;
+            o1.OrtakYazdır();
+
+            BMW b1 = new BMW();
+            Console.WriteLine("--------------BMW--------------");
+            b1.MODEL = "BMW X5";
+            b1.renk = "Kırmızı";
+            b1.OrtakYazdır();
+        }
+    }
+    public class Arac
+    {
+        protected static string arac_türü { get; set; }
+        protected static int yapimyili { get; set; }
+        protected static int id { get; set; }
+
+        public string AracTür { get { return arac_türü; } set { value=this.AracTür; } }
+        public int YapımYIL { get { return yapimyili; } set { yapimyili=value; } }
+        public int ID { get { return id; } set { ID = value; } }
+
+        public Arac()
+        {
+            Console.WriteLine("Araç Kurucu Çalıştı");
+            id++;
+        }
+        public virtual void OrtakYazdır()
+        {
+            Console.WriteLine("Araç Türü: " + arac_türü);
+            Console.WriteLine("id: " + id);
+            Console.WriteLine("Yapım Yılı: " + yapimyili);
+        }
+    }
+    class otomobil : Arac
+    {
+        protected static string marka { get; set; }
+        protected static int agirlik { get; set; }
+        protected static int tekerleksayisi { get; set; }
+
+        public string ARACTUR { get { return arac_türü; } set { arac_türü = value; } }
+
+        public int AGIRLIK
+        { get { return agirlik; } set { agirlik = value; } }
+        public int TEKERSAYI
+        { get { return tekerleksayisi; } set { tekerleksayisi = value; } }
+        public otomobil()
+        {
+            Console.WriteLine("Otomobil Kurucu Çalıştı");
+        }
+        public override void OrtakYazdır()
+        {
+            base.OrtakYazdır();
+            Console.WriteLine("Ağırlık: " + agirlik);
+            Console.WriteLine("Tekerlek Sayısı: " + tekerleksayisi);
+        }
+    }
+    class BMW : otomobil
+    {
+        protected string Model { get; set; }
+        protected string Renk { get; set; }
+
+        public string MODEL { get { return Model; } set { this.Model = value; } }
+        
+        public string renk { get { return Renk; } set { this.Renk = value; } }
+
+        public BMW()
+        {
+            Console.WriteLine("BMW Kurucu Çalıştı");
+        }
+        public override void OrtakYazdır()
+        {
+            base.OrtakYazdır();
+            Console.WriteLine("Model: " + Model);
+            Console.WriteLine("Renk: " + Renk);
+        }
+    }
+}
---
+++ Inheritance2/Inheritance2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+
+namespace Deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personel p1 = new Personel();
+            Console.WriteLine("----------------Personel----------------");
+            p1.ADSOYAD = "Arda Ünsalan";
+            p1.YAS = 20;
+            p1.OrtakYazdır();
+
+            Ogretmen o1 = new Ogretmen();
+            Console.WriteLine("----------------Öğretmen----------------");
+            o1.ADSOYAD = "Mahmut Satici";
+            o1.YAS = 19;
+            o1.DERSSAATI = 15;
+            o1.maasHesabi();
+            o1.OrtakYazdır();
+
+            Ogretmen o2 = new Ogretmen();
+            Console.WriteLine("----------------Öğretmen----------------");
+            o1.ADSOYAD = "Yusuf Üzeyir Kaya";
+            o1.YAS = 20;
+            o1.DERSSAATI = 15;
+            o1.maasHesabi();
+            o1.OrtakYazdır();
+
+            Mudur m1 = new Mudur();
+            Console.WriteLine("----------------Müdür----------------");
+            m1.UNVAN = "Müdür";
+            m1.ADSOYAD = "Mutlucan Gökçukur";
+            m1.DERSSAATI = 10;
+            m1.maasHesabi();
+            m1.OrtakYazdır();
+        }
+    }
+    public class Personel
+    {
+        protected static string adsoyad { get; set; }
+        protected static int yas { get; set; }
+        protected static int personelno { get; set; }
+
+        public string ADSOYAD { get { return adsoyad; } set { adsoyad = value; } }
+        public int YAS { get { return yas; } set { yas = value; } }
+        public Personel()
+        {
+            Console.WriteLine("Personel Kurucusu Çalıştı.");
+            personelno++;
+        }
+        public virtual void OrtakYazdır()
+        {
+            Console.WriteLine("Adı Soyadı: " + adsoyad);
+            Console.WriteLine("Personel Numarası: " + personelno);
+            Console.WriteLine("Yaş: " + yas);
+        }
+    }
+    public class Ogretmen : Personel
+    {
+        protected static string bolum { get; set; }
+        protected static int derssaati { get; set; }
+        protected static int maas { get; set; }
+
+        public string BOLUM { get { return bolum; } set { bolum = value; } }
+        public int DERSSAATI { get { return derssaati; } set { derssaati = value; } }
+        public Ogretmen()
+        {
+            Console.WriteLine("Ogretmen Kurucu Çalıştı");
+        }
+        public virtual int maasHesabi()
+        {
+            maas = derssaati * 960;
+            return maas;
+        }
+        public override void OrtakYazdır()
+        {
+            base.OrtakYazdır();
+            Console.WriteLine("Ders Saati: " + derssaati);
+            Console.WriteLine("Maaş: " + maas);
+        }
+    }
+    public class Mudur : Ogretmen
+    {
+        protected string unvan;
+        public string UNVAN { get { return unvan; } set { unvan = value; } }
+        public Mudur()
+        {
+            Console.WriteLine("Müdür Kurucusu Çalıştı");
+        }
+        public override int maasHesabi()
+        {
+            maas = derssaati * 2000;
+            return maas;
+        }
+        public override void OrtakYazdır()
+        {
+            base.OrtakYazdır();
+            Console.WriteLine("Ünvan: " + unvan);
+        }
+    }
+}
